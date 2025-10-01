0dad466701886901306e85ffc91ec1c339ecf597
hady <abdelhadinodejs@gmail.com>
2025-10-01 14:22:50 +0100

    Add : Clone() and improve cross_validate functionality

--- Logistic_Regression_Scratch.py
+++ Logistic_Regression_Scratch.py
@@ -1,8 +1,5 @@
 import numpy as np
-import random
-
-from numpy.random import beta
-
+import inspect
 
 np.set_printoptions(precision=3, suppress=True, linewidth=100)
 
@@ -11,13 +8,15 @@ class LogisticRegression:
     def __init__(
         self,
         penalty=None,
-        learning_rate=0.001,
+        cv=5,
+        lr=0.001,
         n_itr=2000,
         batch_size=None,
-        tol=1e-6,
+        tol=1e-9,
         C=1.0,
     ):
-        self.lr = self._validate_positive(learning_rate, "learning_rate")
+        self.lr = self._validate_positive(lr, "learning_rate")
+        self.cv = cv
         self.n_itr = int(self._validate_positive(n_itr, "n_itr"))
         self.batch_size = self._validate_positive(batch_size, "batch_size")
         self.penalty = self._validate_penalty(penalty)
@@ -26,6 +25,20 @@ class LogisticRegression:
         self.beta = None
         self.intercept = 0
         self.losses = []
+
+    def get_params(self):
+        return {
+            "penalty": self.penalty,
+            "lr": self.lr,
+            "n_itr": self.n_itr,
+            "batch_size": self.batch_size,
+            "tol": self.tol,
+        }
+
+    def set_params(self, **params):
+        for key, value in params.items():
+            setattr(self, key, value)
+        return self
 
     def _validate_positive(self, value, atrb_name):
         if atrb_name == "batch_size" and value is None:
@@ -56,19 +69,22 @@ class LogisticRegression:
 
     def _sigmoid_function(self, X):
         logit = X @ self.beta + self.intercept
-
-        P_x = 1 / (np.exp(-logit) + 1)
+        P_x = np.where(
+            logit >= 0, 1 / (1 + np.exp(-logit)), np.exp(logit) / (1 + np.exp(logit))
+        )
         return P_x
 
-    def fit(self, X, Y, n):
+    def fit(self, X, Y):
         p = X.shape[1]
+        n = X.shape[0]
         self.beta = np.zeros((p, 1))
         self.intercept = 0
         prev_loss = float("inf")
         for _ in range(self.n_itr):
             if self.batch_size is None:
-                X_GD, Y_GD = X, Y
-                print(self.batch_size)
+                # X_GD, Y_GD = X, Y
+                X_GD = X
+                Y_GD = Y.reshape(-1, 1)
             else:
                 idx = np.random.choice(n, size=self.batch_size, replace=False)
                 X_GD = X[idx]
@@ -108,7 +124,7 @@ class LogisticRegression:
         eps = 1e-15
 
         P_x = np.clip(P_x, eps, 1 - eps)
-        cost_fn = -np.mean(Y.T @ np.log(P_x) + (1 - Y).T @ np.log(1 - P_x))
+        cost_fn = -np.mean(Y.T * np.log(P_x) + (1 - Y).T * np.log(1 - P_x))
 
         return cost_fn
 
@@ -122,6 +138,14 @@ class LogisticRegression:
     def accuracy(self, Y, Y_pred):
         accuracy = np.mean(Y_pred == Y)
         return accuracy
+
+
+def clone(estimator):
+    # Know the class of the estimator
+    cls = estimator.__class__
+    params = estimator.get_params()
+
+    return cls(**params)
 
 
 def generate_dummy_data(n=1000, p=3, seed=1):
@@ -140,93 +164,55 @@ def generate_dummy_data(n=1000, p=3, seed=1):
     return response, coeff_true, intercept_true, X
 
 
-def sigmoid_function(X, beta):
-    logit = X @ beta
-    P_x = 1 / (np.exp(-logit) + 1)
-    return P_x
-
-
-def cross_entropy_loss(X, y, beta):
-    sigmoid_fn = sigmoid_function(X, beta)
-
-    # clipping the array to avoid log(0)
-    eps = 1e-15
-    sigmoid_fn = np.clip(sigmoid_fn, eps, 1 - eps)
-    cost_fn = -np.mean(y.T @ np.log(sigmoid_fn) + (1 - y).T @ np.log(1 - sigmoid_fn))
-    return cost_fn
-
-
-def stochastic_gradient_decent(lr, n_itr, Y, p, X, n):
-    beta_est = np.zeros((p, 1))
+def cross_validate(model, X, Y, seed, shuffle=True, cv=5):
+    n = X.shape[0]
+    if cv > n:
+        raise ValueError(f"Number of folds {cv} with {n} samples ")
     losses = []
-    for i in range(n_itr):
-        # Randomly selecting (x_i,y_i)
-        idx = np.random.choice(n, size=1, replace=False)
-        Xs = X[idx]
-        Ys = Y[idx].reshape(-1, 1)
+    folds = K_folds(n, cv, shuffle, seed)
+    for train_idx, test_idx in folds:
+        # clone the model
+        model_cv = clone(model)
 
-        # Calculting P(X)
-        sigmoid_fn = sigmoid_function(Xs, beta_est)
+        X_train, Y_train = X[train_idx], Y[train_idx]
+        X_test, Y_test = X[test_idx], Y[test_idx]
 
-        # calculating the gradient for the cross entropy loss
-        gradient_cel = Xs.T @ (sigmoid_fn - Ys)
-        beta_est_new = beta_est - (lr * gradient_cel)
+        model_cv.fit(X_train, Y_train)
+        loss = model_cv.cross_entropy_loss(Y_test, X_test)
+        losses.append(loss)
 
-        current_loss = cross_entropy_loss(X, Y, beta_est)
-        losses.append(current_loss)
-
-        if np.linalg.norm(beta_est_new - beta_est) < 1e-6:
-            break
-
-        beta_est = beta_est_new
-
-    return beta_est, losses
+    return losses, np.mean(losses)
 
 
-def mini_batch_gradient_decent(lr, n_itr, batch_size, Y, p, X, n):
-    beta_est = np.zeros((p, 1))
-    losses = []
+def K_folds(n, K, shuffle, seed):
+    n = np.arange(n)
+    if shuffle:
+        rng = np.random.default_rng(seed)
+        rng.shuffle(n)
+    folds = []
+    fold_size = len(n) // K
+    print(fold_size)
+    for i in range(K):
+        start = i * fold_size
+        end = (i + 1) * fold_size
+        test_idx = n[start:end]
+        train_idx = np.concatenate([n[:start], n[end:]])
+        folds.append((train_idx, test_idx))
 
-    for i in range(n_itr):
-        idx = np.random.choice(n, size=batch_size, replace=False)
-        Xb = X[idx]
-        Yb = Y[idx].reshape(-1, 1)
-
-        sigmoid_fn = sigmoid_function(Xb, beta_est)
-
-        gradient_cel = (1 / batch_size) * (Xb.T @ (sigmoid_fn - Yb))
-
-        beta_est_new = beta_est - (lr * gradient_cel)
-        current_loss = cross_entropy_loss(X, Y, beta_est)
-        losses.append(current_loss)
-        if np.linalg.norm(beta_est_new - beta_est) < 1e-6:
-            break
-        beta_est = beta_est_new
-    return beta_est, losses
+    return folds
 
 
-def predict_proba(X, beta):
-    return sigmoid_function(X, beta)
+Y, beta_true, intercept_true, X = generate_dummy_data(n=200, p=3, seed=10)
 
+model = LogisticRegression(lr=0.1, n_itr=800, batch_size=None, C=1, penalty=None)
 
-def predict(X, beta, threshold):
-    predicted_prob = predict_proba(X, beta)
-    predicted_class = (predicted_prob >= threshold).astype(int)
-    return predicted_class, predicted_prob
+beta, loss_train = model.fit(X, Y)
 
+losses, avg_loss = cross_validate(model, X, Y, seed=100, shuffle=True, cv=10)
 
-def accuracy(y, pred_y):
-    return np.mean(y == pred_y)
-
-
-Y, beta_true, intercept_true, X = generate_dummy_data(n=10000, p=10, seed=10)
-
-model = LogisticRegression(
-    learning_rate=0.1, n_itr=20000, batch_size=100, C=2, penalty="L2"
-)
-beta_class, loss_class = model.fit(X, Y, 1000)
 predicted_class_cl, predicted_prob_cl = model.predict(X, 0.5)
 accuracy_class = model.accuracy(Y, predicted_class_cl)
 
-print(beta_class)
-print(beta_true)
+print(beta)
+# print(model_clone
+print(avg_loss)
