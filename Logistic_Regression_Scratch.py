187d4279013b02efe1e22ee736164a0f65e08d9a
hady <abdelhadinodejs@gmail.com>
2025-10-05 22:34:09 +0100

    Refactor .fit() method and add grid_searchCV function

--- Logistic_Regression_Scratch.py
+++ Logistic_Regression_Scratch.py
@@ -11,7 +11,7 @@ class LogisticRegression:
         lr=0.001,
         n_itr=2000,
         batch_size=None,
-        tol=1e-9,
+        tol=1e-6,
         C=1.0,
     ):
         self.lr = self._validate_positive(lr, "learning_rate")
@@ -32,6 +32,7 @@ class LogisticRegression:
             "n_itr": self.n_itr,
             "batch_size": self.batch_size,
             "tol": self.tol,
+            "C": self.C,
         }
 
     def set_params(self, **params):
@@ -68,6 +69,8 @@ class LogisticRegression:
 
     def _sigmoid_function(self, X):
         logit = X @ self.beta + self.intercept
+        logit = np.clip(logit, -500, 500)
+
         P_x = np.where(
             logit >= 0, 1 / (1 + np.exp(-logit)), np.exp(logit) / (1 + np.exp(logit))
         )
@@ -79,6 +82,7 @@ class LogisticRegression:
         self.beta = np.zeros((p, 1))
         self.intercept = 0
         prev_loss = float("inf")
+
         for _ in range(self.n_itr):
             if self.batch_size is None:
                 # X_GD, Y_GD = X, Y
@@ -89,43 +93,47 @@ class LogisticRegression:
                 X_GD = X[idx]
                 Y_GD = Y[idx].reshape(-1, 1)
 
+            if self.penalty == "l2":
+                regularization = (1 / (self.C * 2)) * self.beta
+            elif self.penalty == "l1":
+                regularization = (1 / self.C) * np.sign(self.beta)
+            else:
+                regularization = 0
+
+            gradient_cel = (1 / len(Y_GD)) * (
+                X_GD.T @ (self._sigmoid_function(X_GD) - Y_GD)
+            ) + regularization
+
+            gradient_intercept = (1 / len(Y_GD)) * np.sum(
+                self._sigmoid_function(X_GD) - Y_GD
+            )
+            self.beta = self.beta - (self.lr * gradient_cel)
+            # print(self.beta)
+            self.intercept = self.intercept - (self.lr * gradient_intercept)
+
             loss = self.cross_entropy_loss(Y, X)
             self.losses.append(loss)
 
             if abs(prev_loss - loss) < self.tol:
                 break
             prev_loss = loss
-            if self.penalty == "l2":
-                regularization_l2 = (1 / self.C) * self.beta
-                gradient_cel = (1 / len(Y_GD)) * (
-                    (X_GD.T @ (self._sigmoid_function(X_GD) - Y_GD)) + regularization_l2
-                )
-            elif self.penalty == "l1":
-                regularization_l1 = (1 / self.C) * np.sign(self.beta)
-                gradient_cel = (1 / len(Y_GD)) * (
-                    (X_GD.T @ (self._sigmoid_function(X_GD) - Y_GD)) + regularization_l1
-                )
-
-            else:
-                gradient_cel = (1 / len(Y_GD)) * (
-                    X_GD.T @ (self._sigmoid_function(X_GD) - Y_GD)
-                )
-            gradient_intercept = (1 / len(Y_GD)) * np.sum(
-                self._sigmoid_function(X_GD) - Y_GD
-            )
-            self.beta = self.beta - (self.lr * gradient_cel)
-            self.intercept = self.intercept - (self.lr * gradient_intercept)
 
         return self.beta, self.losses
 
     def cross_entropy_loss(self, Y, X):
+        Y = Y.reshape(-1, 1)
         P_x = self._sigmoid_function(X)
         eps = 1e-15
 
         P_x = np.clip(P_x, eps, 1 - eps)
-        cost_fn = -np.mean(Y.T * np.log(P_x) + (1 - Y).T * np.log(1 - P_x))
-
-        return cost_fn
+        reg_loss = 0
+        cost_fn = -np.mean(Y * np.log(P_x) + (1 - Y) * np.log(1 - P_x))
+        if self.beta is not None:
+            if self.penalty == "l2":
+                reg_loss = (1 / (self.C * 2)) * np.sum(self.beta**2)
+            elif self.penalty == "l1":
+                reg_loss = (1 / self.C) * np.sum(np.abs(self.beta))
+        return cost_fn + reg_loss
 
     def predict(self, X, threshold):
         predicted_probabilities = self._sigmoid_function(
@@ -151,19 +159,17 @@ def generate_dummy_data(n=1000, p=3, seed=1):
     np.random.seed(seed)
 
     X = np.random.randn(n, p)
-
+    # X = (X - np.mean(X, axis=0)) / np.std(X, axis=0)
     coeff_true = np.array([2] * (p))  # (p,)
     intercept_true = 3.0  #
-    P_x = np.exp(X @ coeff_true + intercept_true) / (
-        1 + np.exp(X @ coeff_true + intercept_true)
-    )
+    P_x = 1 / (1 + np.exp(-(X @ coeff_true + intercept_true)))
 
     response = np.random.binomial(1, P_x)
 
     return response, coeff_true, intercept_true, X
 
 
-def cross_validate(model, X, Y, seed, shuffle=True, cv=5):
+def cross_validate(model, X, Y, seed=10, shuffle=True, cv=5):
     n = X.shape[0]
     if cv > n:
         raise ValueError(f"Number of folds {cv} with {n} samples ")
@@ -191,7 +197,6 @@ def K_folds(n, K, shuffle, seed):
         rng.shuffle(n)
     folds = []
     fold_size = len(n) // K
-    print(fold_size)
     for i in range(K):
         start = i * fold_size
         # when n %K !=0 the last fold takes all the reminder
@@ -234,17 +239,72 @@ def stratified_K_folds(n, K, y, shuffle, seed):
     return folds
 
 
-Y, beta_true, intercept_true, X = generate_dummy_data(n=200, p=3, seed=10)
+def grid_searchCV(model, param_grid, cv, X, Y):
+    lr_values = param_grid.get("lr_values")
+    C_values = param_grid.get("C_values")
+    print(lr_values)
+    print(C_values)
 
-model = LogisticRegression(lr=0.1, n_itr=800, batch_size=None, C=1, penalty=None)
+    score_cv = {}
+    best_params = None  # best coefficients
+    best_estimator = None  # best model
+    best_score = np.inf  # best CV score
 
-beta, loss_train = model.fit(X, Y)
+    for lr_value in lr_values:
+        for C_value in C_values:
+            model_CV = clone(model)
+            model_CV.C = C_value
+            model_CV.lr = lr_value
+            print(f"the value of lr is :{lr_value} the value of C is : {C_value} ")
+            loss, avg_loss = cross_validate(model_CV, X, Y, cv=cv)
+            score_cv[(lr_value, C_value)] = avg_loss
+            print(f"{avg_loss}< {best_score}")
+            if avg_loss < best_score:
+                best_score = avg_loss
+                best_estimator = model_CV
+                best_params = model_CV.get_params()
+                print(best_params)
+        # print(f"for C={C_value} and lr={lr_value} the avg loss of the model is ")
 
-losses, avg_loss = cross_validate(model, X, Y, seed=100, shuffle=True, cv=10)
+        #  print(avg_loss)
+        #  print("---------")
 
-predicted_class_cl, predicted_prob_cl = model.predict(X, 0.5)
-accuracy_class = model.accuracy(Y, predicted_class_cl)
+    # Train the best model on the full data set
+    print("the best score is :", best_score)
+    print("the best params are : ", best_params)
+    if (best_estimator is not None) and (best_params is not None):
+        best_estimator.lr = best_params["lr"]
+        best_estimator.C = best_params["C"]
+        beta, loss = best_estimator.fit(X, Y)
+        print(f"the best beta is {beta}")
+    return {
+        "best_score": best_score,
+        "best_model": best_estimator,
+        "best_params": best_params,
+        "score_cv": score_cv,
+    }
 
-print(beta)
-# print(model_clone
-print(avg_loss)
+
+Y, beta_true, intercept_true, X = generate_dummy_data(n=300, p=3, seed=24)
+
+model = LogisticRegression(lr=0.1, n_itr=900, batch_size=30, C=0.01, penalty=None)
+beta, loss = model.fit(X, Y)
+losses, avg_loss = cross_validate(model, X, Y, seed=10, shuffle=True, cv=10)
+param_grid = {
+    "lr_values": [0.1, 0.01, 0.001],
+    "C_values": [0.01, 0.1, 0.2, 0.3],
+}
+score = grid_searchCV(model, param_grid, 10, X, Y)
+modelCV = score["best_model"]
+# beta, loss_train = model.fit(X, Y)
+# print(beta)
+
+# model = score["best_model"]
+
+
+print(f"the best (base model) betas are : {beta}")
+print(f"the base model avg loss is : {avg_loss}")
+# print(f"the best model avg_loss is {avg_loss}")
+# predicted_class_cl, predicted_prob_cl = model.predict(X, 0.5)
+# accuracy_class = model.accuracy(Y, predicted_class_cl)
+# print(accuracy_class)
